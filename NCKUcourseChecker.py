607334dcc9078637dab3dc49379d7bb0799162bd
LeeW <cl87654321@gmail.com>
2014-02-17 21:22:07 +0800

    successfully print course but still cannot find a way to format properly

--- NCKUcourseChecker.py
+++ NCKUcourseChecker.py
@@ -1,6 +1,7 @@
 import NCKUcourseParser
 import urllib.request
 from operator import itemgetter
+from tabulate import tabulate
 
 
 if __name__ == '__main__':
@@ -16,18 +17,22 @@ if __name__ == '__main__':
     for line in webContent.splitlines():
         parser.feed(line)
 
-    filterCondition = ["系號", "序號", "課程名稱", "學分", "教師姓名", "餘額 ", "時間"]
+    # filterCondition = ["系號", "序號", "課程名稱(連結課程地圖)", "學分", "教師姓名*:主負責老師", "餘額 ", "時間"]
+    filterCondition = ["系號", "序號", "課程名稱(連結課程地圖)",  "餘額 ", "時間"]
     filteredIndex = []
     title = parser.getTitle()
     for i in range(len(title)):
-        if title[i][0] in filterCondition:
+        if title[i] in filterCondition:
             filteredIndex.append(i)
 
     filteredCourseInfo = []
     for i in range(len(parser.getContent())):
-        filteredCourseInfo.append([parser.getContent()[i][j] for j in filteredIndex])
+        if parser.getContent()[i][15] > '0':
+            filteredCourseInfo.append([parser.getContent()[i][j] for j in filteredIndex])
 
-    filteredCourseInfo = sorted(filteredCourseInfo, key=itemgetter(5))
-    formatStr = "{0}\t{1}\t{2}\t\t{3}\t{4}\t{5}\t{6}"
-    for i in range(len(filteredCourseInfo)):
-        print (formatStr.format(*filteredCourseInfo[i]))
+    filteredCourseInfo = sorted(filteredCourseInfo, key=itemgetter(3), reverse=True)
+    formatStr = "{0:2}\t{1:3}\t{2:25s}\t{3}\t{4}"
+    for i in filteredCourseInfo:
+        print (formatStr.format(*i))
+
+    # print (tabulate(filteredCourseInfo, headers=filterCondition, tablefmt='pipe'))
