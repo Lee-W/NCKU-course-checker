36b903838ba160fa4619c5348b0a7bf9e5f504ed
LeeW <cl87654321@gmail.com>
2014-02-17 15:54:39 +0800

    modify checker

--- NCKUcourseChecker.py
+++ NCKUcourseChecker.py
@@ -1,7 +1,10 @@
 import NCKUcourseParser
 import urllib.request
 from operator import itemgetter
+
+
 if __name__ == '__main__':
+
     departmentNo = input("請輸入系所代碼 : ")
 
     URL = "http://140.116.165.74/qry/qry001.php?dept_no=" + departmentNo
@@ -13,17 +16,18 @@ if __name__ == '__main__':
     for line in webContent.splitlines():
         parser.feed(line)
 
-    filterCondition = ["系號","序號","課程名稱(連結課程地圖)","學分","教師姓名*:主負責老師","餘額 ","時間","教室"]
+    filterCondition = ["系號", "序號", "課程名稱", "學分", "教師姓名", "餘額 ", "時間"]
     filteredIndex = []
-    for i in range(len(parser.titles)):
-        if parser.titles[i] in filterCondition:
+    title = parser.getTitle()
+    for i in range(len(title)):
+        if title[i][0] in filterCondition:
             filteredIndex.append(i)
 
     filteredCourseInfo = []
-    for i in range(len(parser.courses)):
-        filteredCourseInfo.append([parser.courses[i][j] for j in filteredIndex])
+    for i in range(len(parser.getContent())):
+        filteredCourseInfo.append([parser.getContent()[i][j] for j in filteredIndex])
 
-    print (filterCondition)
     filteredCourseInfo = sorted(filteredCourseInfo, key=itemgetter(5))
+    formatStr = "{0}\t{1}\t{2}\t\t{3}\t{4}\t{5}\t{6}"
     for i in range(len(filteredCourseInfo)):
-        print (filteredCourseInfo[i][5])
+        print (formatStr.format(*filteredCourseInfo[i]))
