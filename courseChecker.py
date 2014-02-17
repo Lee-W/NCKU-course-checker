11d103c09f2cd104fd3e89969035d04b44bb50da
LeeW <cl87654321@gmail.com>
2014-02-17 09:51:25 +0800

    transform into python 3 version and successfully parse a course but not yet format

--- courseChecker.py
+++ courseChecker.py
@@ -1,123 +1,67 @@
-# -*- coding: utf-8 -*-  
+'''
+enviroment : python 3.3.2
+'''
+# -*- coding: utf-8 -*-
 
-import urllib  
-import HTMLParser  
+from html.parser import HTMLParser
+import urllib.request
 import sys
 
-added = False
-formStart = False
-columnStart = False
-result = [] 
-course = []
+class courseParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.titleStart = False
+        self.columnStart = False
+        self.courseStart =False
+        self.title = ""
+        self.titles = []
+        self.courseInfo = []
+        self.courses = []
 
-title = [ "系所名稱" , "系號" , "序號" , "課程碼" ,
-	  "分班碼" , "班別" , "年級" , "類別" ,
-	  "英語授課" , "課程名稱" , "選必修" , "學分" ,
-	  "教師姓名" , "已選課人數 " , "餘額 " , "時間" ,
-	  "教室" , "備註" , "限選條件" , "業界專家參與" , "屬性碼" , "跨領域學分學程" ] 
+    def handle_data(self, data):
+        if self.titleStart == True:
+            self.title += data
+        elif self.titleStart == False and self.title!="":
+            self.titles.append(self.title)
+            self.title = ""
 
-class courseHTMLParser(HTMLParser.HTMLParser):  
+        if self.courseStart == True:
+            self.courseInfo.append(data)
 
-	def handle_starttag(self, tag, attrs): 
-		global  added , course , formStart ,columnStart
+    def handle_starttag(self, tag, attrs):
+        if tag == "th" and self.titleStart == False:
+            self.titleStart = True
 
-		if tag == 'tr':
-			formStart = True
-			course = []
-		if tag == 'td' :
-			columnStart = True
-			added = False
-	
-	def handle_data(self, data):
-		global added , course, formStart ,columnStart
-		
-		if formStart == True:
-			if data.strip() == "":
-				added = True
-				course.append(u' ')
-			else:
-				if added ==True and columnStart == True:
-					course[ len(course)-1 ] += " " + data.strip()
-				else:	
-					added = True
-					course.append( data.strip() )
-	
-	def handle_endtag(self, tag): 
-		global added , course , formStart ,columnStart , result
+        if tag == "tr" and self.courseStart == False and len(self.titles) > 0:
+            self.courseStart = True
 
-		if tag == 'tr':
-			formStart = False
-			result.append(course)
-		if tag == 'td':
-			column = False
-			if added == False:
-				course.append(u' ')
-  
-	
-	def unknown_decl(self, data):
-		"""Override unknown handle method to avoid exception"""  
-        pass  
+        if tag == "td" and self.columnStart == False:
+            self.columnStart = True
 
-def mainProcess( dNo ): 
+    def handle_endtag(self, tag):
+        if tag == "th" and self.titleStart == True:
+            self.titleStart = False
 
-	courseWeb = urllib.urlopen("http://140.116.165.74/qry/qry001.php?dept_no="+dNo)  
-	webContent = courseWeb.read().decode('utf_8')  
-	courseWeb.close()  
-	Parser = courseHTMLParser()  
-  
-	try:  
-		for line in webContent.splitlines():  
-			if hasattr(Parser, 'stop') and Parser.stop:  
-				break  
-			Parser.feed(line)  
-	except HTMLParser.HTMLParseError, data:  
-		print "# Parser error : " + data.msg  
-  
-	Parser.close()  
+        if tag == "tr" and self.courseStart == True:
+            self.courses.append(self.courseInfo)
+            self.courseInfo = []
+            self.courseStart = False
 
-	result[0] = title
-
-	printList=[1,2,9,10,11,12,13,14,15]
-
-	for i in range ( 0 , len(result) ):
-		for j in range ( 0 , len(result[i]) ):
-			for k in range ( 0 , len(printList) ):
-				if j is not printList[k]:
-					continue			
-	
-				if i is 0:
-					print result[i][j]+"\t",
-					if j is printList[len(printList)-1]:
-						print "\n",
-
-				elif result[i][14].find(u'\u6eff') is -1 :
-					if j is 9:
-						print "%7s\t" %result[i][j][0:min(7,len(result[i][j]))],
-						continue
-					if j is 12:
-						if len (result[i][j]) > 3:
-							print "%3s*\t\t" %result[i][j][0:3],
-						else:
-							print "%3s\t\t" %result[i][j],
-						continue
-					if j is 14:
-					 	print "\t",
-
-					print result[i][j]+"\t",
-
-					if j is printList[len(printList)-1]:
-						print "\n",
-			
+        if tag == "td" and self.courseStart == True:
+            self.columnStart = False
 
 
 
-departmentNo = raw_input('請輸入系所代碼:')
+if __name__ == '__main__':
+    URL = "http://140.116.165.74/qry/qry001.php?dept_no=AN"
+    web = urllib.request.urlopen(URL)
+    webContent = web.read().decode("utf8")
+    web.close()
 
-while( departmentNo!= "bye" ):
-	mainProcess( departmentNo )
-	added = False
-	formStart = False
-	columnStart = False
-	result = [] 
-	course = []
-	departmentNo = raw_input('\n如果完成了請輸入bye\n請輸入系所代碼:')
+    parser = courseParser()
+    for line in webContent.splitlines():
+        parser.feed(line)
+
+    for i in parser.courses:
+        print (i)
+        print ()
