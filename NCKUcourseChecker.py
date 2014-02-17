58563efd22ee53dbc43030f4121a242b54a1f591
LeeW <cl87654321@gmail.com>
2014-02-17 15:02:52 +0800

    fix parser bug

--- NCKUcourseChecker.py
+++ NCKUcourseChecker.py
@@ -1,9 +1,10 @@
 import NCKUcourseParser
 import urllib.request
-import sys
-
+from operator import itemgetter
 if __name__ == '__main__':
-    URL = "http://140.116.165.74/qry/qry001.php?dept_no=AN"
+    departmentNo = input("請輸入系所代碼 : ")
+
+    URL = "http://140.116.165.74/qry/qry001.php?dept_no=" + departmentNo
     web = urllib.request.urlopen(URL)
     webContent = web.read().decode("utf8")
     web.close()
@@ -12,6 +13,17 @@ if __name__ == '__main__':
     for line in webContent.splitlines():
         parser.feed(line)
 
-    for i in parser.courses:
-        print (i)
-        print ()
+    filterCondition = ["系號","序號","課程名稱(連結課程地圖)","學分","教師姓名*:主負責老師","餘額 ","時間","教室"]
+    filteredIndex = []
+    for i in range(len(parser.titles)):
+        if parser.titles[i] in filterCondition:
+            filteredIndex.append(i)
+
+    filteredCourseInfo = []
+    for i in range(len(parser.courses)):
+        filteredCourseInfo.append([parser.courses[i][j] for j in filteredIndex])
+
+    print (filterCondition)
+    filteredCourseInfo = sorted(filteredCourseInfo, key=itemgetter(5))
+    for i in range(len(filteredCourseInfo)):
+        print (filteredCourseInfo[i][5])
--- NCKUcourseParser.py
+++ NCKUcourseParser.py
@@ -1,50 +1,53 @@
 '''
 enviroment : python 3.3.2
 '''
-# -*- coding: utf-8 -*-
-
 from html.parser import HTMLParser
+
 
 class courseParser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
         self.titleStart = False
-        self.columnStart = False
-        self.courseStart =False
         self.title = ""
         self.titles = []
+
+        self.columnStart = False
+        self.courseStart = False
+        self.tmpText = ""
         self.courseInfo = []
         self.courses = []
 
     def handle_data(self, data):
-        if self.titleStart == True:
+        if self.titleStart is True:
             self.title += data
-        elif self.titleStart == False and self.title!="":
+        elif self.titleStart is False and self.title != "":
             self.titles.append(self.title)
             self.title = ""
 
-        if self.courseStart == True:
-            self.courseInfo.append(data)
+        if self.columnStart is True:
+            if data == '額滿':
+                self.tmpText = 0
+            elif data.isdigit():
+                self.tmpText = int(data)
+            else:
+                self.tmpText = data
 
     def handle_starttag(self, tag, attrs):
-        if tag == "th" and self.titleStart == False:
+        if tag == "td" and self.columnStart is False and self.courseStart is True:
+            self.columnStart = True
+        elif tag == "th" and self.titleStart is False:
             self.titleStart = True
-
-        if tag == "tr" and self.courseStart == False and len(self.titles) > 0:
+        elif tag == "tr" and self.courseStart is False and len(self.titles) > 0:
             self.courseStart = True
 
-        if tag == "td" and self.columnStart == False:
-            self.columnStart = True
-
     def handle_endtag(self, tag):
-        if tag == "th" and self.titleStart == True:
+        if tag == "td" and self.columnStart is True:
+            self.columnStart = False
+            self.courseInfo.append(self.tmpText)
+            self.tmpText = ""
+        elif tag == "th" and self.titleStart is True:
             self.titleStart = False
-
-        if tag == "tr" and self.courseStart == True:
+        elif tag == "tr" and self.courseStart is True:
+            self.courseStart = False
             self.courses.append(self.courseInfo)
             self.courseInfo = []
-            self.courseStart = False
-
-        if tag == "td" and self.courseStart == True:
-            self.columnStart = False
-
