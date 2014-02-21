3552348430f93361aa4c6931728021903cc0ea5e
LeeW <cl87654321@gmail.com>
2014-02-21 18:33:18 +0800

    extract NCKUcourseParse

---
+++ HTMLFormParser.py
@@ -0,0 +1,55 @@
+'''
+enviroment : python 3.3.2
+'''
+from html.parser import HTMLParser
+
+
+class HTMLFormParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.rowStart = False
+        self.columnStart = False
+        self.tmpColInOneRow = []
+        self.tmpRow = []
+        self.table = []
+
+    def handle_data(self, data):
+        if self.columnStart is True:
+            self.tmpColInOneRow.append(data)
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "tr" and self.rowStart is False:
+            self.rowStart = True
+        elif tag == "th" or tag == "td" and self.columnStart is False:
+            self.columnStart = True
+
+    def handle_endtag(self, tag):
+        if tag == "tr" and self.rowStart is True:
+            self.rowStart = False
+            self.table.append(self.tmpRow)
+            self.tmpRow = []
+        elif tag == "th" or tag == "td" and self.columnStart is True:
+            self.columnStart = False
+            self.tmpRow.append("".join(self.tmpColInOneRow))
+            self.tmpColInOneRow = []
+
+    def getHeader(self):
+        return self.table[0]
+
+    def getContent(self):
+        return self.table[1:]
+
+    def getTable(self):
+        return self.table
+
+
+if __name__ == '__main__':
+    import urllib.request
+    web = urllib.request.urlopen("http://lee-w.github.io/HTMLFormParser/")
+    webContent = web.read().decode("utf8")
+    web.close()
+
+    parser = HTMLFormParser()
+    for line in webContent.splitlines():
+        parser.feed(line)
+    print (parser.getTable())
--- NCKUcourseParser.py
+++ NCKUcourseParser.py
@@ -1,18 +1,10 @@
 '''
 enviroment : python 3.3.2
 '''
-from html.parser import HTMLParser
+from HTMLFormParser import HTMLFormParser
 
 
-class courseParser(HTMLParser):
-    def __init__(self):
-        HTMLParser.__init__(self)
-        self.rowStart = False
-        self.columnStart = False
-        self.tmpColInOneRow = []
-        self.tmpRow = []
-        self.table = []
-
+class NCKUcourseParser(HTMLFormParser):
     def handle_data(self, data):
         if self.columnStart is True:
             if data == '額滿':
@@ -20,45 +12,13 @@ class courseParser(HTMLParser):
             else:
                 self.tmpColInOneRow.append(data)
 
-    def handle_starttag(self, tag, attrs):
-        if tag == "tr" and self.rowStart is False:
-            self.rowStart = True
-        elif tag == "th" and self.columnStart is False:
-            self.columnStart = True
-        elif tag == "td" and self.columnStart is False:
-            self.columnStart = True
-
-    def handle_endtag(self, tag):
-        if tag == "tr" and self.rowStart is True:
-            self.rowStart = False
-            self.table.append(self.tmpRow)
-            self.tmpRow = []
-        elif tag == "th" and self.columnStart is True:
-            self.columnStart = False
-            self.tmpRow.append("".join(self.tmpColInOneRow))
-            self.tmpColInOneRow = []
-        elif tag == "td" and self.columnStart is True:
-            self.columnStart = False
-            self.tmpRow.append("".join(self.tmpColInOneRow))
-            self.tmpColInOneRow = []
-
-    def getTitle(self):
-        return self.table[0]
-
-    def getContent(self):
-        return self.table[1:]
-
-    def getTable(self):
-        return self.table
-
-
 if __name__ == '__main__':
     import urllib.request
     web = urllib.request.urlopen("http://140.116.165.74/qry/qry001.php?dept_no=AN")
     webContent = web.read().decode("utf8")
     web.close()
 
-    parser = courseParser()
+    parser = NCKUcourseParser()
     for line in webContent.splitlines():
         parser.feed(line)
     print (parser.getTable())
