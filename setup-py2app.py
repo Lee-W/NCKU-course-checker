84e9345ae7a96b6513cb182a83eaf30ba0815fae
LeeW <cl87654321@gmail.com>
2015-02-05 16:58:02 +0800

    add setup-py2app

---
+++ setup-py2app.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+main_python_file = "GUI.py"
+application_title = "NCKU Course Checker"
+application_description = "App to query NCKU courses"
+
+includes = ["NCKU_course_checker.NCKU_course_checker"]
+excludes = []
+packages = []
+include_files = []
+build_exe_options = {"includes": includes,
+                     "excludes": excludes,
+                     "packages": packages,
+                     "include_files": include_files}
+
+setup(app=["GUI.py"],
+      setup_requires=["py2app"],
+      name=application_title,
+      version="0.2",
+      description=application_description,
+      author="LeeW",
+      author_email="cl87654321@gmail.com",
+      url="https://github.com/Lee-W/NCKU-course-checker",
+      options={"build_exe":  build_exe_options},
+      )
