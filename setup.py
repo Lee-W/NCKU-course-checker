8fecd2c9e17af22b62fb59c3caa5b9b2bc1ef8f2
LeeW <cl87654321@gmail.com>
2015-02-05 16:18:27 +0800

    add more info to setup

--- setup.py
+++ setup.py
@@ -1,22 +1,30 @@
-application_title = "NCKU_course_checker"
-main_python_file = "GUI.py"
-
 import sys
 
 from cx_Freeze import setup, Executable
 
-build_exe_options = {"includes": ["tkinter", "NCKU_course_checker.NCKU_course_checker"]}
+main_python_file = "GUI.py"
+application_title = "NCKU Course Checker"
+application_description = "App to query NCKU courses"
+
+includes = ["tkinter", "NCKU_course_checker.NCKU_course_checker"]
+excludes = []
+packages = []
+include_files = []
+build_exe_options = {"includes": includes,
+                     "excludes": excludes,
+                     "packages": packages,
+                     "include_files": include_files}
 
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-includes = ["atexit","re"]
-
-setup(
-        name = application_title,
-        version = "0.1",
-        description = "Sample cx_Freeze Tkinter script",
-        options = {"build_exe" : {"includes" : includes }},
-        executables = [Executable(main_python_file, base = base)])
-
+setup(name=application_title,
+      version="0.2",
+      description=application_description,
+      author="LeeW",
+      author_email="cl87654321@gmail.com",
+      url="https://github.com/Lee-W/NCKU-course-checker",
+      options={"build_exe":  build_exe_options},
+      executables=[Executable(main_python_file, base=base)]
+      )
